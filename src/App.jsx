3484f5766fa24b3541d8e9bf4274ae6c8a68b7ac
pc <pc@pcs-MacBook-Pro.local>
2023-04-13 20:57:50 +0400

    added function to copy outcome password

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
@@ -27,13 +28,16 @@ function App() {
     }
   }, [data]);
 
+  const [showElement, setShowElement] = useState(false);
   const [password, setPassword] = useState("");
-
   const [passwordLength, setPasswordLength] = useState('');
   const [includeUppercase, setIncludeUppercase] = useState(false);
   const [includeLowercase, setIncludeLowercase] = useState(false);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [includeSymbols, setIncludeSymbols] = useState(false);
+
+
+  
 
   const handleGeneratePassword = (e) => {
     let characterList = "";
@@ -63,33 +67,61 @@ function App() {
     return password;
   };
 
-  const copytoFun = () => {
- 
-  };
-  const handleCopyPassword = (e) => {
-    copytoFun();
+
+
+  // has to improve
+  const handleClick = () => {
+    setShowElement(!showElement);
+    
+
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowElement(false);
+    }, 3000); // hide the element after 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+  
+  const handleCopyPassword = () => {
+    navigator.clipboard.writeText(password);
+    handleClick();
+    useEffect();
+
   };
 
+
+
+
+
+  const [copyValue, setCopyValue] = useState("") 
   return (
     <div className="App">
       <div className="container">
         <div className="generator">
           <h2 className="generator-header">Password Generator</h2>
           <div className="generator-password">
-            <h3 className="fieldText">{password}</h3>
+           <h3 className="fieldText"> {password}
+            </h3>
+              <p className="copied-comment">
+              {showElement && <p>COPIED</p>}</p>
             <button onClick={handleCopyPassword} className="copy-btn">
-              <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
+            <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
                 <path
                   className="copied"
                   d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
                   fill="#A4FFAF"
                 />
               </svg>
+
             </button>
           </div>
 
           <div className="output-container">
+
             <label className="sliderLabel" for="slider">
+
               Character Length{""}
               <span className="sliderValue" id="sliderValue">
                 <p>{data}</p>
@@ -206,13 +238,12 @@ function App() {
                 ></div>
               </div>
             </div>
-
-            <div className="generate">
             <button onClick={handleGeneratePassword} className="generator-btn">
+
               Generate 
+            <svg className="arrow-icon" width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg>
             </button>
-            
-            </div>
+
           </div>
         </div>
       </div>
